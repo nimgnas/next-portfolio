1293f42adf4967b4ad0209d04f4aea1ddd01264c
nimgnas <sangminyn@gmail.com>
2023-07-13 19:36:24 +0900

    Design: tailwind config 컬러 확장, 기본컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        lightGreen: "#14CF73",
+        primaryGreen: "#052E1A",
+        darkGreen: "#01190E",
+        primaryWhite: "#E0EBE5",
       },
     },
   },
   plugins: [],
-}
+};
