434433b215f4da49214b26c615775ce02f61ac8d
nimgnas <sangminyn@gmail.com>
2023-07-24 23:29:11 +0900

    add: Icon, IconLoader 기본 세팅

---
+++ src/app/Icon/Icon.tsx
@@ -0,0 +1,31 @@
+interface IconProps {
+  name: IconName;
+  width: number;
+  height: number;
+  // tailwind css className custom을 위한 property
+  // stroke, fill 적용 불가, background만 가능
+  className?: React.ComponentProps<"div">["className"];
+}
+
+/**
+ * @example
+ * <Icon name="arrow-down" width={24} height={24} />
+ */
+const Icon = ({ name, width, height, className }: IconProps) => {
+  return (
+    <svg
+      style={{ pointerEvents: "none" }}
+      width={width.toString()}
+      height={height.toString()}
+      //   stroke="none"
+      className={className}
+    >
+      <use href={`#${name}`} />
+    </svg>
+  );
+};
+
+export default Icon;
+
+// IconLoader의 id 값을 타이핑해야합니다.
+export type IconName = "arrow-down" | "next" | "vercel";
---
+++ src/app/Icon/IconLoader.tsx
@@ -0,0 +1,12 @@
+const IconLoader = () => (
+  <svg
+    id="sprite-symbols"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/symbol"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    style={{ display: "none" }}
+  >
+    <defs id="defs"></defs>
+  </svg>
+);
+export default IconLoader;
